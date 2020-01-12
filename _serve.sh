d84819bfd67b0dac5e40251d48e4e89eeb861302
Chrizmo <chrizmo@gmail.com>
2020-01-12 17:28:29 +0100

    Added exclusion of files and made testing script simple.

--- _serve.sh
+++ _serve.sh
@@ -1,3 +1,3 @@
 
 echo "Starting Jekyll"
-docker run --rm -v "$PWD:/src" -p 4000:4000 --name bsides -it jekyll/builder jekyll build
+docker run --rm -v "$PWD:/srv/jekyll" -p 4000:4000 --name bsides -it jekyll/builder jekyll serve
